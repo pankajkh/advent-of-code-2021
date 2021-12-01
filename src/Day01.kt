57b78d0e18cc6a099ebe93d419b33d13cd5b2c3b
Pankaj Khandelwal <Pankaj.Khandelwal@pharmeasy.in>
2021-12-01 11:44:52 +0530

    advent-of-code-2021:Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var totalIncreased = 0
+        for (i in (1 until input.size)) {
+            if (input[i].toInt() > input[i-1].toInt())
+                ++totalIncreased
+        }
+        return totalIncreased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var totalIncreased = 0
+        for (i in (3 until input.size)) {
+            if (input[i].toInt() > input[i-3].toInt())
+                ++totalIncreased
+        }
+        return totalIncreased
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
